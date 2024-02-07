001902aea1dc36f91135e9a219a7abdc97c89162
Guilherme Rodrigues Tavares <guilherme.rtavares@outlook.com>
2024-02-07 09:26:36 -0300

    Home page finished

--- front/src/App.js
+++ front/src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import HomePage from './pages/home';
 import NavBar from './components/navBar';
 import SideBar from './components/sideBar';
-import Download from './testPages/download';
 import GameScreen from './testPages/gamescreen';
 import { Route, Routes } from 'react-router-dom';
 
@@ -15,7 +14,6 @@ function App() {
       <NavBar />
       <Routes>
         <Route path='/home' element={<HomePage />} />
-        <Route path='/download' element={<Download />} />
         <Route path='/game' element={<GameScreen />} />
       </Routes>
     </>
